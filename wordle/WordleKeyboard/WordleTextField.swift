d6f4d520c60a7c1b3c5cfa54fd58ad892485de02
Zahra Kermani <zahrapkermani@gmail.com>
2022-02-05 19:14:20 +0100

    Override deletBackward for WordleTextField

--- wordle/WordleKeyboard/WordleTextField.swift
+++ wordle/WordleKeyboard/WordleTextField.swift
@@ -8,8 +8,18 @@
 import Foundation
 import UIKit
 
+protocol WordleTextFieldDelegate: AnyObject {
+    func textFieldDidDelete()
+}
+
 class WorldleTextField: UITextField, WordleKeyboardObserver {
+    weak var myDelegate: WordleTextFieldDelegate?
     func add(_ string: String) {
         self.text?.append(string)
+    }
+    
+    override func deleteBackward() {
+        super.deleteBackward()
+        myDelegate?.textFieldDidDelete()
     }
 }
