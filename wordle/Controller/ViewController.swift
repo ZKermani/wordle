61d5340c5f7a35e198ecda30438ead4ca4adac5b
Zahra Kermani <zahrapkermani@gmail.com>
2022-03-15 09:45:20 +0100

    Re-design Play button

--- wordle/Controller/ViewController.swift
+++ wordle/Controller/ViewController.swift
@@ -9,10 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         AppUtility.lockOrientation(.portrait)
+        
+        playButton.layer.cornerRadius = playButton.frame.height / 10
     }
 
     @IBAction func startButtonPressed(_ sender: UIButton) {
